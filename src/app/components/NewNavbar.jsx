42f8989df5104ca4f071a376951e703eb918f7f3
Shahzaib Awan <awan.shahzaib.0314@gmail.com>
2025-07-19 19:27:25 +0500

    added text field

--- src/app/components/NewNavbar.jsx
+++ src/app/components/NewNavbar.jsx
@@ -4,6 +4,7 @@ export const NewNavbar = () => {
   return (
     <div className='p-6 bg-amber-200'>
         <p className='text-[24px] text-red-600 font-bold'>lorem ipsum dolor sit amet</p>
+        <p>Testing this pge for deployment</p>
     </div>
   )
 }
