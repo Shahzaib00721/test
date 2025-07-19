29c1739683a23d8249cda9f6ddf13a801ac4796e
Shahzaib Awan <awan.shahzaib.0314@gmail.com>
2025-07-19 19:19:08 +0500

    first commit

---
+++ src/app/components/NewNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NewNavbar = () => {
+  return (
+    <div className='p-6 bg-amber-200'>
+        <p className='text-[24px] text-red-600 font-bold'>lorem ipsum dolor sit amet</p>
+    </div>
+  )
+}
